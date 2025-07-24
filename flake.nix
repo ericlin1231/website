92e3b2b8dc141507f8236a45ce238f8742496183
tzuchilin <eric1231.tw@gmail.com>
2025-07-24 22:44:04 +0800

    init

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "My Website Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "my-website-ts-py-shell";
+          packages = with pkgs; [
+            deno
+            uv
+            python313
+          ];
+          shellHook = ''
+			export UV_VENV_PATH=./backend/.venv
+			if [ ! -d "$UV_VENV_PATH" ]; then
+				echo "[devShell] create uv venv -> $UV_VENV_PATH"
+				uv venv "$UV_VENV_PATH"
+				uv pip install -r backend/pyproject.toml
+			fi
+			source "$UV_VENV_PATH/bin/activate"
+		  '';
+        };
+      }
+    );
+}
